af6d20562177769f21c272a564c36c040521809d
WilliamOrl <62764335+WilliamOrl@users.noreply.github.com>
2021-09-20 18:41:46 -0300

    Update Newton.c

--- Newton.c
+++ Newton.c
@@ -2,41 +2,36 @@
 #include <stdlib.h>
 
 //Funções do programa
-void secantes(int n, double X0, double X1, int e, int MAXITER,int ind);
-void print(double* Xk,double* Fx, double X0, double X1, int max,int ind);
+void newton(int n, double X0, int e, int MAXITER, int ind);
+void print(double* Xk,double* Fx,double* devFx, double X0, int max, int ind);
 
 
 //Inicio
 int main() {   
-    int n, max, e, ind;
-    double x0, x1; 
-    
+    int n, max, e, ind=0;
+    double x0; 
+
     while (1)
     {
-    
-    ind++;   
-    printf("Para encerar o programa Digite o Grau da funcao como 0\n");
-    printf("Digite o grau da funcao: ");
-    scanf("%d",&n);
-    if(n==0)
-        return 0;  
-    
-    printf("Digite o valor de X0: ");
-    scanf("%lf",&x0);
-    
-    printf("Digite o valor de X1: ");
-    scanf("%lf",&x1);
+     ind++;   
+     printf("Para encerar o programa Digite o Grau da funcao como 0\n");
+     printf("Digite o grau da funcao: ");
+     scanf("%d",&n);
+     if(n==0)
+         return 0;
 
-    printf ("Digite o Maxitermo: ");
-    scanf("%d",&max);
+     printf("Digite o valor de X0: ");
+     scanf("%lf",&x0);
     
-    printf("Digite o erro 'e' talque erro = 10^-e: ");
-    scanf("%d",&e);
+     printf ("Digite o Maxitermo: ");
+     scanf("%d",&max);
+    
+     printf("Digite o erro 'e' talque erro = 10^-e: ");
+     scanf("%d",&e);
 
-    secantes(n, x0, x1, e, max, ind);
-
+     newton(n, x0, e, max,ind);
     }
-    
+
     
     
     return 0;
@@ -47,12 +42,12 @@ int main() {
 //                  Função de Newton
 //============================================================
 
-void secantes(int n, double X0, double X1, int e, int MAXITER,int ind){
+void newton(int n, double X0, int e, int MAXITER, int ind){
 
     int F[n];
     int i,j, max;
     double prod= 0, erro; 
-    double respX0 = 0,respX1 = 0,X;
+    double respdevx = 0, respx = 0;
     double x[2];
     long int parada;
     double minerro = 1;
@@ -61,7 +56,7 @@ void secantes(int n, double X0, double X1, int e, int MAXITER,int ind){
     
     double* Xk;
     double* Fx;
-    double* Ex;
+    double* devFx;
     
 
     //Definindo o erro
@@ -77,110 +72,95 @@ void secantes(int n, double X0, double X1, int e, int MAXITER,int ind){
         scanf("%d",&F[i]);
     }
 
-    //Aplicando o das Secantes
+    //Aplicando o método de Newton
     x[0] = X0;
-    x[1]= X1; 
+    x[1]= 0; 
     prod = 0;
     max = 0;
     erro = 0;
-
-    do{      
-     max++; 
-     respX0 = 0;
-     respX1 = 0;
-
-
-     //Calculando F(X0)
-     for (i=0; i<=n; i++){    
-         if(i==0){
-            respX0 = F[i];
-         }else{
-             for(j=0; j<i; j++){       
-                 if(j==0)
-                    prod = x[0];
-             
-                 if (j!=0){
-                    prod = prod*x[0];
-                 }
-             }
-         respX0 = respX0 + F[i]*prod;
-        } 
-     }
-
-
-     //Calculando F(X1)
-     for (i=0; i<=n; i++){    
-        if(i==0){
-            respX1 = F[i];
-        }else{
-             for(j=0; j<i; j++){       
-                if(j==0)
-                prod = x[1];
-             
-                if (j!=0){
-                prod = prod*x[1];
-                 }
-             }
-        respX1 = respX1 + F[i]*prod;
-        } 
-     }
-
-     //Alocando os valores na memória para serem escritos  
-     if(max==1){
-          Xk = malloc(1*sizeof(double));
-          Fx = malloc(1*sizeof(double));      
-          Ex = malloc(1*sizeof(double));   
-     }else{
-          Xk = realloc(Xk,max*sizeof(double));            
-          Fx = realloc(Fx,max*sizeof(double));  
-          Ex = realloc(Ex,max*sizeof(double));
-     }
-
-     //Calculando o método de Newton           
-     X = ((respX1*x[0]) - (respX0*x[1]))/(respX1 - respX0);
-     x[0]= x[1];
-     x[1] = X;
-     respX1 = 0;
-
-     
-     //Calculando para ver se encontrou a raiz
-     for (i=0; i<=n; i++){    
-        if(i==0){
-            respX1 = F[i];
-        }else{
-             for(j=0; j<i; j++){       
-                if(j==0)
-                prod = x[1];
-             
-                if (j!=0){
-                prod = prod*x[1];
-                 }
-             }
-        respX1 = respX1 + F[i]*prod;
-        } 
-     }
-
-     Xk[max-1] = X; 
-     Fx[max-1] = respX1;
-     Ex[max-1] = erro;
-     
-
-     // Se encontrar a raiz de X
-     parada = respX1*1000000;
-     if (parada == 0){ 
-         print(Xk,Fx,X0,X1,max, ind);
-         return;
-     }
-
-
-     //Caso não encontre a raiz
-     erro = (x[0]-x[1])/x[0];
-     if(erro<0)
-        erro = -erro;
     
-    }while (max<=MAXITER && erro > minerro); 
+    do{    
+        respx = 0;
+        respdevx =0;
+        x[1]= x[0];
+        max ++;
+        
+         
+        //Calculo do Fx
+        for (i=0; i<=n; i++){    
+            if(i==0){
+              respx = F[i];
+              }else{
+                 for(j=0; j<i; j++){       
+                      if(j==0)
+                      prod = x[0];
+            
+                       if (j!=0){
+                         prod = prod*x[0];
+                        }
+                      }
+                 respx = respx + F[i]*prod;
+                } 
+        }
+
+        //Calculo da Derivada de Fx
+        for (i=0; i<=n; i++){    
+            if(i==0){
+            respdevx = 0;
+            }else{
+                for(j=0; j<i-1; j++){       
+                    if(j==0)
+                        prod = x[0];
+            
+                    if (j!=0){
+                         prod = prod*x[0];
+                    }
+                }
+                 
+                if(i==1){
+                        respdevx = F[i];
+                }else{
+                        respdevx = respdevx + F[i]*prod*(i);       
+                }       
+              } 
+        }
+
+
+        //Calculando o método de Newton
+        x[0] = x[0] - (respx/respdevx);
+
+        //Alocando os valores na memória para serem escritos  
+        if(max==1){
+            Xk = malloc(1*sizeof(double));
+            Fx = malloc(1*sizeof(double));  
+            devFx = malloc(1*sizeof(double));               
+        }else{
+            Xk = realloc(Xk,max*sizeof(double));            
+            Fx = realloc(Fx,max*sizeof(double));  
+            devFx = realloc(devFx,max*sizeof(double));  
+        }
+                  
+        Xk[max-1] = x[0];
+        Fx[max-1] = respx;
+        devFx[max-1] = respdevx;
+        
+
+        // Se encontrar a raiz de X
+        parada = respx*1000000;
+        if (parada == 0){ 
+            print(Xk,Fx,devFx,X0,max,ind);
+            return;
+        }
+
+
+        //Caso não encontre a raiz
+        erro = (x[0]-x[1])/x[0];
+        if(erro<0)
+            erro = -erro;
     
-    print(Xk,Fx,X0,X1,max, ind);
+    }while (max<=MAXITER && erro > minerro);
+    
+    print(Xk,Fx,devFx,X0,max,ind);
     return;
 }
 
@@ -189,19 +169,18 @@ void secantes(int n, double X0, double X1, int e, int MAXITER,int ind){
 //                  Criação do .txt
 //============================================================
 
-void print(double* Xk,double* Fx, double X0, double X1, int max,int ind){
+void print(double* Xk,double* Fx,double* devFx,double X0, int max, int ind){
 
-    int i; 
+    int i;  
     double erro;
-    char nome[30]; 
+    char nome[30];
     FILE *pont_arq;
     
     //abrindo o arquivo
     printf("Criando Arquivo...\n");
-    sprintf(nome,"Secantes<%d>.txt",ind);
+    sprintf(nome,"Newton<%d>.txt",ind);
    
     pont_arq = fopen(nome, "w");
-
 
     if(pont_arq==NULL){
         printf("Falha ao Gerar o Arquivo");
@@ -212,17 +191,17 @@ void print(double* Xk,double* Fx, double X0, double X1, int max,int ind){
     fputs("Vitor Ferreira Paschoal n°11215532 \nWilliam Carrara Orlato n°1180091\n\n",pont_arq);
 
     //Adicionando os Dados no Arquivo
-    fprintf(pont_arq,"x(0) = %.6lf\nx(1) = %.6lf\n\n\n",X0,X1); 
+    fprintf(pont_arq,"x(0) = %.6lf\n\n\n",X0); 
     
     //Escrevendo Xk, Fx e Fx' no aquivo
-    fprintf(pont_arq,"      x          Fx         erro\n");
+    fprintf(pont_arq,"      x          Fx         Fx'        erro\n");
     for(i=0;i<max;i++){
         if(Fx[i]<0)
             Fx[i] = Fx[i]*-1;
         erro = Xk[i]-Xk[max-1];
         if (erro<0)
-            erro= -erro;               
-        fprintf(pont_arq,"x(%d): %.6lf   %.6lf   %.6lf\n",i+2,Xk[i],Fx[i],erro);
+            erro= -erro;              
+        fprintf(pont_arq,"x(%d): %.6lf   %.6lf   %.6lf   %.6lf\n",i+1,Xk[i],Fx[i],devFx[i],erro);
     }
    
     // fechando arquivo
